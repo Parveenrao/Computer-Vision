c421bb5ca7c359e665d70e415810e08cf0cda72b
Parveen <parveeenrao7727@gmail.com>
2026-05-27 10:53:55 +0530

    RTSP intro

---
+++ RTSP/0Intro.py
@@ -0,0 +1,33 @@
+"""
+=> RTSP (Real Time Streaming Protocol)
+
+   -> RTSP  is a network protocol used to control streaming media servers 
+
+   -> It do not carry the video itself , it control the stream (play and pause)
+
+   -> Actual video is sent using RTP (Real-Time Transport Protocol)
+
+
+   -> Where RTSP
+      
+      1. CCTC / IP cameras
+      2. Security monitoring system 
+      3. Live video streaming apps
+
+      4. Media servers 
+
+   -> RTSP URL Format 
+
+       rtsp://username:password@IP_ADDRESS:PORT/stream_path
+
+       Default port 554
+
+
+   -> Key concept in RTSP 
+
+      1. Describe -> get stream info 
+      2. Setup    -> prepare connection 
+      3. Play     -> start stream 
+      4. Pause    -> pause stream                
+
+"""
