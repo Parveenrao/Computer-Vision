2f0fe86b7d5d52be4a9695cf8d27c838b1bdd5d2
Parveen <parveeenrao7727@gmail.com>
2026-05-27 10:52:37 +0530

    Basic FFmpeg

---
+++ FFmpeg/0Intro.py
@@ -0,0 +1,27 @@
+"""  
+
+=> FFmpeg 
+    
+    -> Is a command line tool working for working with video , audio , formats
+
+    -> It is an open source multimedia framework  that can 
+      
+       1. Convert audio/video formats
+       2. Compress video
+       3. Cut and merge clips
+       4. Extract audio from video 
+       5. Record screens and streams
+       6. Resize , crop and filter media
+       7. Stream live video
+       8, Work with subtitles , thumbnails and more
+
+    -> Basic Syntax 
+
+         ffmpeg [global options] -i input [output options] output    
+
+         i -> input files 
+
+         input -> input files 
+
+         output -> output files
+"""
