292cf126104e4d715bf572b5a03d3e99dc4bc28f
Parveen <parveeenrao7727@gmail.com>
2026-05-27 14:48:23 +0530

    Basic PyTorch

---
+++ Pytorch/Learning/0Intro.py
@@ -0,0 +1,28 @@
+"""
+=> Pytorch
+   
+    -> Pytorch is a open source python library used for machine learning and deep learning
+
+    -> Helps you build system that can learn from data 
+
+    -> Why Pytroch is popular 
+
+      1. Easy to understand 
+      
+      2. Flexible
+
+      3. Dynamic Computation 
+
+         -> Pytorch builds computation step-by-step , not a fixed graph , debugging is easier
+
+      4. GPU support
+
+    -> Core Idea of Pytorch 
+
+       1. Tensors 
+
+       2. Automatic Differentiation (Autograd)    
+
+       3. Neural Network modules          
+
+"""
