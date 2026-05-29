d8bde86e74521534d0a025770dc554168a3f43bb
Parveen <parveeenrao7727@gmail.com>
2026-05-29 11:17:38 +0530

    Gstreamer intro

---
+++ GStreamer/0Intro.py
@@ -0,0 +1,33 @@
+""" 
+
+=> GStreamer 
+  
+    -> Is one of the most important multimedia framework used in linux , embedded system , Andriod ,
+       video analytics , streaming service , camera , AI Pipeline , conferencing apps and 
+       media players
+
+---------------------------------------------------------------------------------------------------
+
+=> What is GStreamer 
+  
+   -> Pipeline based multimedia framework 
+
+   -> It helps developers build system that 
+    
+        1. Play audio , video 
+
+        2. Record from camera/microphone 
+
+        3. Strem media over network 
+
+        4. Encode/decode video 
+
+        5. Process AI / video analytics 
+
+        6. Mix, transfrom , filter media 
+
+        7. Build-real time media application 
+
+
+
+"""
