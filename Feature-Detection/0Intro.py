b7f31be64029e9f9f9b618efb154efb51bd98b54
Parveen <parveeenrao7727@gmail.com>
2026-06-02 15:49:56 +0530

    Feature Detection

---
+++ Feature-Detection/0Intro.py
@@ -0,0 +1,28 @@
+""" 
+=> Feature Detection 
+
+    -> Converting an image into useful numverical information (features) that a computer can understand 
+       and use for task for like tracking , detecting , classification and OCR 
+
+    -> Image = millions of pixels 
+
+    -> Fetaure Extraction = finding important patterns from thos pixels 
+
+    -> Features = edges, corners , texture shape , object part etc
+
+
+    -> Example , Suppose u have image of cat 
+
+        Raw Image = Pixel Value = 224 * 224 * 3
+
+        After feature extraction 
+
+        1. Whisker detected 
+        2. Ear shape detected 
+        3. Eye pattern detected 
+        4. Fur texture detected 
+
+        The model use these features to detect Cat     
+
+
+"""
