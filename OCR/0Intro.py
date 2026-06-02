32476c88c57a747ed9a71c3d19483faddff296cc
Parveen <parveeenrao7727@gmail.com>
2026-06-02 15:50:42 +0530

    OCR Intro

---
+++ OCR/0Intro.py
@@ -0,0 +1,67 @@
+""" 
+=> OCR (Optical Character Recognition) 
+
+   -> It is a computer vision technology that converts text present in image , scanned documents ,
+      scanned documents , PDFs, camera feeds or handwritten documents into machine - readable and
+      editable text 
+
+  -> OCR allow computer to read text from  an image just like humans read text from book
+
+
+  -> Example 
+
+      Suppose An image containing 
+
+       Invoice No: 12345
+       Amount: ₹5000  
+
+
+     OCR convert this image into 
+
+       Invoice No: 12345
+       Amount: ₹5000
+
+     So the computer can search , edit , store or analyze this text 
+
+-----------------------------------------------------------------------------------
+
+=> Application Of OCR 
+
+    1. Document Digitization 
+
+        -> convert book into digital text 
+        -> Archiving old documents 
+
+    2. Automatic Number Plate Recognition (ANPR)
+
+        -> Reading vehicle number plates 
+
+   3. Invoice Processing 
+
+       -> Extract invoice number , amount , date 
+
+   4. Bank Cheque Processing  
+
+       -> Reading accounts numbers and amounts 
+
+   5. ID card Recognition
+
+      -> Aadhar Card 
+
+      -> PAN card 
+
+      -> passport 
+
+   6. Reciept Scanning Apps
+
+       -> Expense tracking application 
+
+   7. Traffic Monitoring 
+
+      -> Reading Road signs and vehicles paltes 
+
+   8. CCTV And Surveillance 
+
+      -> Detecting text from books , banners , containers and vehicles                                          
+
+"""
