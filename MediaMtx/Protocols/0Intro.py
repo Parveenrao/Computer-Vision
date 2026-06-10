83d45c42ce388308bb1485f83c2601cb15b4926f
Parveen <parveeenrao7727@gmail.com>
2026-06-10 17:31:30 +0530

    Protocols

---
+++ MediaMtx/Protocols/0Intro.py
@@ -0,0 +1,15 @@
+""" 
+
+=> What is Protocol
+  
+    -> Why Protocol exist 
+
+    -> Imagine a camera generate video , Camera -> Video frames 
+
+    How do we send those frames over network
+
+    we need rules 
+
+    Those rules are called protocols
+
+"""
