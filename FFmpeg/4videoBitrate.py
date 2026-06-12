a13b043ecffd571ae4ef15d7a11fa98274cd667d
Parveen <parveeenrao7727@gmail.com>
2026-06-12 12:05:30 +0530

    FFmpeg

---
+++ FFmpeg/4videoBitrate.py
@@ -0,0 +1,40 @@
+"""  
+
+=> VideoBitrate 
+    
+   -> Amount of data used to represent one second of video, usually measured in kbps or mbps
+
+   -> in simple terms it tells , how much information is packed into each second of video
+
+   -> Bitrate effects 
+       
+      a. Higher Birate 
+          
+         -> Better image quality 
+         -> Larger file size 
+         -> Require more internet speed to stream 
+
+      b. Lower bitrate 
+
+        -> Lower quality 
+        -> Smaller file size 
+        -> Easy to stream on slow connection
+
+
+    -> A video at 2mbps use 2 megabits of data every second
+    -> A video at 10mbps use much more data , specially for HD
+
+
+    -> Mobile videos use low bitrate to save data'
+
+    -> 4K videos need high bitrate(15-50mbps)        
+
+--------------------------------------------------------------------------------------------
+
+=> Bitrate vs. Resolution 
+   
+   Resolution -> no. of pixels (1080 , 4k)
+
+   Bitrate   -> how much data is used to describe those pixels
+
+"""
